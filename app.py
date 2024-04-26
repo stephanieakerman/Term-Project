1251d13be8b2a9d90c75e5bb7e80c52ce7747f2e
stephanieakerman <157422375+stephanieakerman@users.noreply.github.com>
2024-04-26 10:48:39 -0500

    update

--- app.py
+++ app.py
@@ -10,13 +10,22 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/readme')
+@app.route('/instructions')
 def readme():
-    return render_template('readme.html')
+    return render_template('instructions.html')
+
+@app.route('/project-evolution')
+def project_evolution():
+    return render_template('project-evolution.html')
+
+@app.route('/attribution')
+def attribution():
+    return render_template('attribution.html')
 
 @app.route('/results')
 def results():
     return render_template('results.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
